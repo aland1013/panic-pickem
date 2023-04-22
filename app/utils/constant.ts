92ad034dff2fb014dfaa5ad9507ae7a21665cece
Adam Land <adam.land@tokensoft.io>
2023-04-22 07:48:00 -0400

    bump bustout date

--- app/utils/constant.ts
+++ app/utils/constant.ts
@@ -2,5 +2,5 @@ export const NUM_PICKS = 5
 
 // TODO: update this value after every show
 // TODO: automate this
-// 99th show before 4/21/23, all songs with LTP before this are bustouts
-export const BUSTOUT_DATE = '2019-12-30'
+// 99th show before 4/22/23, all songs with LTP before this are bustouts
+export const BUSTOUT_DATE = '2019-12-31'
