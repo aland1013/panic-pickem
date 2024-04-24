b792a7f0d1f207e8fe122164d87b090fb966779a
Adam Land <adam.land@gmail.com>
2024-04-24 19:42:14 -0400

    bump bustout date

--- app/utils/constant.ts
+++ app/utils/constant.ts
@@ -2,5 +2,5 @@ export const NUM_PICKS = 5
 
 // TODO: update this value after every show
 // TODO: automate this
-// 99th show before 4/14/24, all songs with LTP before this are bustouts
-export const BUSTOUT_DATE = '2021-10-30'
+// 99th show before 4/25/24, all songs with LTP before this are bustouts
+export const BUSTOUT_DATE = '2021-11-19'
